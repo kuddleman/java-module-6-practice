e46a8794c830217057cbeedea3bf0b868d920705
Kuddleman <edl1155@gmail.com>
2021-03-12 14:26:01 -0800

    add ZipCodeProcessor class

---
+++ src/ZipCodeProcessor.java
@@ -0,0 +1,7 @@
+public class ZipCodeProcessor {
+    public static void main(String[] args) {
+        int[] zipCodes = new int[5];
+        int validCodes = 0;
+        int invalidCodes = 0;
+    }
+}
